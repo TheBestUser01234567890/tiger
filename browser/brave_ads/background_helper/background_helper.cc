3179fb8cf156d7e67ba69a9c7e76f91a417da9cd
TheBestUser01234567890 <98957578+TheBestUser01234567890@users.noreply.github.com>
2023-06-17 22:50:51 +0300

    Update background_helper.cc

--- browser/brave_ads/background_helper/background_helper.cc
+++ browser/brave_ads/background_helper/background_helper.cc
@@ -3,9 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/brave_ads/background_helper/background_helper.h"
+#include "tiger/browser/brave_ads/background_helper/background_helper.h"
 
-#include "brave/browser/brave_ads/background_helper/background_helper_holder.h"
+#include "tiger/browser/brave_ads/background_helper/background_helper_holder.h"
 #include "build/build_config.h"
 
 namespace brave_ads {

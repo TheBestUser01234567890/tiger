52b7abeb70748200bd6918e145eeda98fba17783
TheBestUser01234567890 <98957578+TheBestUser01234567890@users.noreply.github.com>
2023-06-18 12:01:13 +0300

    delete unneded things

--- chromium_src/android_webview/browser/aw_permission_manager.cc
+++ chromium_src/android_webview/browser/aw_permission_manager.cc
@@ -5,22 +5,5 @@
 
 #include "components/permissions/permission_util.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
-
-#define NUM                                             \
-  BRAVE_ADS:                                            \
-  case PermissionType::BRAVE_COSMETIC_FILTERING:        \
-  case PermissionType::BRAVE_TRACKERS:                  \
-  case PermissionType::BRAVE_HTTP_UPGRADABLE_RESOURCES: \
-  case PermissionType::BRAVE_FINGERPRINTING_V2:         \
-  case PermissionType::BRAVE_SHIELDS:                   \
-  case PermissionType::BRAVE_REFERRERS:                 \
-  case PermissionType::BRAVE_COOKIES:                   \
-  case PermissionType::BRAVE_SPEEDREADER:               \
-  case PermissionType::BRAVE_ETHEREUM:                  \
-  case PermissionType::BRAVE_SOLANA:                    \
-  case PermissionType::BRAVE_GOOGLE_SIGN_IN:            \
-  case PermissionType::BRAVE_LOCALHOST_ACCESS:          \
-  case PermissionType::NUM
-
 #include "src/android_webview/browser/aw_permission_manager.cc"
-#undef NUM
+
